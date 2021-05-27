22276fb7dd670fa6f3181130a923f8842a64257a
PatriotRossii <patriotrossii2019@mail.ru>
2021-05-27 19:44:22 +0500

    Fix clippy lints, formatting

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-#[macro_use] extern crate rocket;
-#[macro_use] extern crate serde;
+#[macro_use]
+extern crate rocket;
+#[macro_use]
+extern crate serde;
 
+use rocket::{http::Status, response::Redirect, Response, Rocket, State};
+use rusqlite::Connection;
 use std::sync::Mutex;
-use rocket::{Response, Rocket, State, http::Status, response::Redirect};
-use rusqlite::{Connection, Error};
 
 use rocket_contrib::json::{Json, JsonValue};
 
@@ -21,10 +23,14 @@ fn generate(db_conn: State<DbConn>, message: Json<GenerateRequest>) -> JsonValue
     let link = message.link.as_str();
     let shortened_id = uuid::Uuid::new_v4().to_string();
 
-    db_conn.lock().expect("Failed to lock").execute(
-        "INSERT INTO urls(shortened_id, original_url) VALUES(?, ?)",
-        &[shortened_id.as_str(), link]
-    ).expect("Failed to insert link");
+    db_conn
+        .lock()
+        .expect("Failed to lock")
+        .execute(
+            "INSERT INTO urls(shortened_id, original_url) VALUES(?, ?)",
+            &[shortened_id.as_str(), link],
+        )
+        .expect("Failed to insert link");
     rocket_contrib::json!({
         "id": shortened_id.as_str(),
         "status": "ok",
@@ -33,15 +39,16 @@ fn generate(db_conn: State<DbConn>, message: Json<GenerateRequest>) -> JsonValue
 
 #[get("/<id>")]
 fn redirect(db_conn: State<DbConn>, id: String) -> Result<Redirect, Response> {
-    let original_url: Result<String, rusqlite::Error> = db_conn.lock().expect("Failed to lock").query_row(
-        "SELECT original_url FROM urls WHERE shortened_id = ?",
-        &[id.as_str()],
-        |row| row.get(0)
-    );
-    
+    let original_url: Result<String, rusqlite::Error> =
+        db_conn.lock().expect("Failed to lock").query_row(
+            "SELECT original_url FROM urls WHERE shortened_id = ?",
+            &[id.as_str()],
+            |row| row.get(0),
+        );
+
     match original_url {
         Err(_) => Err(Response::build().status(Status::NotFound).finalize()),
-        Ok(e) => Ok(Redirect::to(format!("{}", e.as_str()))),
+        Ok(e) => Ok(Redirect::to(e)),
     }
 }
 
