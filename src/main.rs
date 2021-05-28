0d2a4dc91dc4e4388476a5cae2c2e1e061a9ea93
PatriotRossii <patriotrossii2019@mail.ru>
2021-05-28 17:48:56 +0500

    Add internal error handling

--- src/main.rs
+++ src/main.rs
@@ -18,29 +18,34 @@ struct GenerateRequest {
     link: String,
 }
 
+fn internal_error() -> Response<'static> {
+    Response::build().status(
+        Status::InternalServerError
+    ).finalize()
+}
+
 #[post("/api/generate", format = "json", data = "<message>")]
-fn generate(db_conn: State<DbConn>, message: Json<GenerateRequest>) -> JsonValue {
+fn generate(db_conn: State<DbConn>, message: Json<GenerateRequest>) -> Result<JsonValue, Response> {
     let link = message.link.as_str();
     let shortened_id = uuid::Uuid::new_v4().to_string();
 
     db_conn
         .lock()
-        .expect("Failed to lock")
+        .map_err(|_| internal_error())?
         .execute(
             "INSERT INTO urls(shortened_id, original_url) VALUES(?, ?)",
             &[shortened_id.as_str(), link],
-        )
-        .expect("Failed to insert link");
-    rocket_contrib::json!({
+        ).map_err(|_| internal_error())?;
+    Ok(rocket_contrib::json!({
         "id": shortened_id.as_str(),
         "status": "ok",
-    })
+    }))
 }
 
 #[get("/<id>")]
 fn redirect(db_conn: State<DbConn>, id: String) -> Result<Redirect, Response> {
     let original_url: Result<String, rusqlite::Error> =
-        db_conn.lock().expect("Failed to lock").query_row(
+        db_conn.lock().map_err(|_| internal_error())?.query_row(
             "SELECT original_url FROM urls WHERE shortened_id = ?",
             &[id.as_str()],
             |row| row.get(0),
